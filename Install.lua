4dc8164c63f8a76e01756d036a49d736179799f5
BananaTeddy <hering.maximilian@web.de>
2022-02-26 14:12:39 +0100

    added install script

--- Install.lua
+++ Install.lua
@@ -1,1 +1,20 @@
--- TODO: Install Script for API
+-- [ Install BT_API]
+
+local baseUrl = 'https://raw.githubusercontent.com/'
+local gitHubUser = 'BananaTeddy'
+local project = 'BT_API'
+local branch = 'main'
+local files = {
+    BT_API = 'BT_API.lua',
+    Turtle = 'Turtle.lua',
+    branches = 'Branches.lua',
+    whitelist = 'whitelist.txt',
+    blacklist = 'blacklist.txt',
+    fluids = 'fluids.txt',
+    fuels = 'fuels.txt'
+}
+
+for alias, file in pairs(files) do
+    local url = baseUrl .. gitHubUser .. '/' .. project .. '/' .. branch .. '/' .. file
+    shell.run('wget ' .. url .. ' ' .. alias)
+end
