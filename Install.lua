e927b373fe46676e2cf111b314f0e1d143237340
BananaTeddy <hering.maximilian@web.de>
2024-09-03 09:57:45 +0200

    updated install file

--- Install.lua
+++ Install.lua
@@ -1,18 +1,27 @@
--- [ Install BT_API]
+-- [ Install BananaCore]
 
-local baseUrl = 'https://raw.githubusercontent.com/'
-local gitHubUser = 'BananaTeddy'
-local project = 'BT_API'
-local branch = 'main'
+local baseUrl = "https://raw.githubusercontent.com/"
+local gitHubUser = "BananaTeddy"
+local project = "BananaCore"
+local branch = "main"
 local files = {
-    BT_API = 'BT_API.lua',
-    Turtle = 'Turtle.lua',
-    branches = 'Branches.lua',
-    whitelist = 'whitelist.txt',
-    blacklist = 'blacklist.txt',
-    fluids = 'fluids.txt',
-    fuels = 'fuels.txt',
-    cobble = 'Cobble.lua'
+    ["BananaCore"] = "BananaCore.lua",
+
+    ["Data/Data"] = "Data/Data.lua",
+    ["Data/File"] = "Data/File.lua",
+
+    ["Number/Number"] = "Number/Number.lua",
+
+    ["Network/Network"] = "Network/Network.lua",
+    ["Network/Packet"] = "Network/Packet.lua",
+    ["Network/Server"] = "Network/Server.lua",
+    ["Network/Client"] = "Network/Client.lua",
+
+    ["Logger/Logger"] = "Logger/Logger.lua",
+
+    ["GUI/GUI"] = "GUI/GUI.lua",
+    ["GUI/PrintUtil"] = "GUI/PrintUtil.lua",
+    ["GUI/ProgressBar"] = "GUI/ProgressBar.lua",
 }
 
 for alias, file in pairs(files) do
